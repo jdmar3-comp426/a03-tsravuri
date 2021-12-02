6770f2f9555fa03c8c031f68c758621803d66532
Tejas Ravuri <tsravuri@gmail.com>
2021-12-02 13:30:28 -0500

    delete extra line

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -52,7 +52,6 @@ export function searchMpg(car_data, minCity, minHighway) {
  * @returns {[]} array of cars
  */
 export function searchName(car_data, searchTerm) {
-    const searchTerm = term.toLowerCase();
     return car_data
         .filter((car) => car.id.toLowerCase().indexOf(searchTerm) !== -1)
         .sort((a,b) => a.id.toLowerCase().indexOf(searchTerm) - b.id.toLowerCase().indexOf(searchTerm));
